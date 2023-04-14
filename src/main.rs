ffd23486070372136161db377633c992843798fe
Zourunfa <473023989@qq.com>
2023-04-14 09:23:58 +0800

    feat:重构(上)

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,35 @@
 use std::env; //collect
 use std::fs;
 
+// 二进制程序关注点分离的指导性原则
+// 将程序拆分为main.rs和lib.rs 将业务逻辑放入lib.rs
+// 当命令行解析逻辑较少的时候，将它放在main.rs
+// 当命令行逻辑变复杂是，需要将main.rs的逻辑抽离到lib.rs
+
 fn main() {
     let args: Vec<String> = env::args().collect();
+    let config = Config::new(&args);
 
-    let query = &args[1];
-    let filename = &args[2];
+    println!("Search for {}", config.query);
+    println!("In file {}", config.filename);
 
-    println!("Search for {}", query);
-    println!("In file {}", filename);
-
-    let contents = fs::read_to_string(filename).expect("Something went wrong reading the file");
+    let contents =
+        fs::read_to_string(config.filename).expect("Something went wrong reading the file");
 
     println!("With text:\n{}", contents); //没发处理非法的unicode字符  // env::args_os()这个可以处理
+}
+
+struct Config {
+    query: String,
+    filename: String,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Config {
+        let query = args[1].clone();
+        let filename = args[2].clone();
+
+        // 返回一个元组
+        Config { query, filename }
+    }
 }
