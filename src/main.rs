038347ed305d08f7c44f8afb80cc56a97604a281
Zourunfa <473023989@qq.com>
2023-04-14 09:08:42 +0800

    feat: 读取文件

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,16 @@
 use std::env; //collect
+use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    println!("{:?}", args); //没发处理非法的unicode字符  // env::args_os()这个可以处理
+    let query = &args[1];
+    let filename = &args[2];
+
+    println!("Search for {}", query);
+    println!("In file {}", filename);
+
+    let contents = fs::read_to_string(filename).expect("Something went wrong reading the file");
+
+    println!("With text:\n{}", contents); //没发处理非法的unicode字符  // env::args_os()这个可以处理
 }
