e8229e964bd5d86b5d5c4a4359fb34d97c901668
Zourunfa <473023989@qq.com>
2023-04-26 19:44:22 +0800

    feat:错误处理

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::env; //collect
 use std::fs;
+use std::process;
 
 // 二进制程序关注点分离的指导性原则
 // 将程序拆分为main.rs和lib.rs 将业务逻辑放入lib.rs
@@ -8,7 +9,11 @@ use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let config = Config::new(&args);
+    // unwrap_or_else是如果Result的返回失败，就会返回穿进去的闭包
+    let config = Config::new(&args).unwrap_or_else(|err|{
+      println!("Probelm parsing arguments: {}",err);
+      process::exit(1);
+    });
 
     println!("Search for {}", config.query);
     println!("In file {}", config.filename);
@@ -25,11 +30,16 @@ struct Config {
 }
 
 impl Config {
-    fn new(args: &[String]) -> Config {
+    // 返回一个Result枚举，如果操作成功就返回config,若是失败返回一个字符串
+    fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough argument");
+        }
+
         let query = args[1].clone();
         let filename = args[2].clone();
 
         // 返回一个元组
-        Config { query, filename }
+        Ok(Config { query, filename })
     }
 }
